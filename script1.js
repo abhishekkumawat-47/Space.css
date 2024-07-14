d1e2caa770251f1f64a7f486115cd2412dd688c8
abhishekkumawat-47 <abhishekkumawat1008@gmail.com>
2024-07-14 22:45:37 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,21 @@
+
+  function createStars() {
+    const container = document.querySelector("body");
+    for (let i = 0; i < 1000; i++) {
+      // Increase the number of stars to 1000
+      const star = document.createElement("div");
+      star.className = "star";
+      star.style.width = ".1px";
+      star.style.height = ".1px";
+      star.style.top = Math.random() * 100 + "%";
+      star.style.left = Math.random() * 100 + "%";
+      container.appendChild(star);
+      setTimeout(() => {
+        container.removeChild(star)
+      }, 1000);
+    }
+  }
+
+setInterval(() => {
+  createStars();
+}, 1000);
